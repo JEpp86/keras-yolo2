cb3b6a4d79e74c3c98293153a4838f054334a3f5
JEpp86 <jasonepp0@gmail.com>
2021-04-19 17:49:13 -0700

    minor update to demonstration script. added demo video for ease of demonstration

--- run_demo.sh
+++ run_demo.sh
@@ -1,3 +1,3 @@
-#python3 docking_demo.py -cfg TiniestV2_model.cfg -w TiniestV2_weights.hd5 -src video -video dockvideo.avi    
+python3 docking_demo.py -cfg TiniestV2_model.cfg -w TiniestV2_weights.hd5 -src video -video dockvideo.avi    
 
-python3 docking_demo.py -cfg yolo_best_model.cfg -w yolo_best_weights.hd5 -src video -video dockvideo.avi    
+#python3 docking_demo.py -cfg yolo_best_model.cfg -w yolo_best_weights.hd5 -src video -video dockvideo.avi    
